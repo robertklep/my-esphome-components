43674bdc24d98debeced01830646bbcb6ee7b8f5
Robert Klep <robert@klep.name>
2025-01-12 13:57:39 +0100

    Not using "publish once" option for now, moved sensor logging to DEBUG level

--- components/delta_solivia/delta-solivia-inverter.cpp
+++ components/delta_solivia/delta-solivia-inverter.cpp
@@ -15,10 +15,10 @@ DeltaSoliviaInverter::DeltaSoliviaInverter(uint8_t address, uint8_t variant) {
 
     // set handlers
     parser->set_publish_sensor([this](const std::string& name, float value) {
-      this->publish_sensor(name, value);
+      this->publish_sensor(name, value, false);
     });
     parser->set_publish_text_sensor([this](const std::string& name, const std::string& value)  {
-      this->publish_text_sensor(name, value);
+      this->publish_text_sensor(name, value, false);
     });
   } else {
     ESP_LOGE(LOG_TAG, "INVERTER%u - invalid variant %u", address_, variant_);
@@ -31,8 +31,7 @@ void DeltaSoliviaInverter::update_sensors(const uint8_t* buffer) {
 }
 
 void DeltaSoliviaInverter::publish_sensor(const std::string& name, float value, bool once) {
-  ESP_LOGI(LOG_TAG, "INVERTER#%u - sensor '%s', value = %f", address_, name.c_str(), value);
-#if 0
+  ESP_LOGD(LOG_TAG, "INVERTER#%u - sensor '%s', value = %f", address_, name.c_str(), value);
   auto entry= sensors_.find(name);
 
   if (entry != sensors_.end()) {
@@ -40,20 +39,17 @@ void DeltaSoliviaInverter::publish_sensor(const std::string& name, float value, 
       entry->second->publish_state(value);
     }
   }
-#endif
 }
 
 void DeltaSoliviaInverter::publish_text_sensor(const std::string& name, const std::string& value, bool once) {
-  ESP_LOGI(LOG_TAG, "INVERTER#%u - sensor '%s', value = %s", address_, name.c_str(), value.c_str());
-#if 0
+  ESP_LOGD(LOG_TAG, "INVERTER#%u - sensor '%s', value = %s", address_, name.c_str(), value.c_str());
   auto entry= text_sensors_.find(name);
 
-  if (entry != textSensors_.end()) {
+  if (entry != text_sensors_.end()) {
     if (! once || ! entry->second->has_state()) {
       entry->second->publish_state(value);
     }
   }
-#endif
 }
 
 }
